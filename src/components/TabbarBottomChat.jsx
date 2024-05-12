18c37ead61a9b1f4f6b8c92cb4d421ba0e6da1ad
vikhangit <khang01279567920@gmail.com>
2024-05-13 05:50:52 +0700

    1

--- src/components/TabbarBottomChat.jsx
+++ src/components/TabbarBottomChat.jsx
@@ -23,12 +23,12 @@ export default function TabbarBottomChat({ active = "home" }) {
   return (
     <div
       className={`fixed bottom-0 w-full z-50 ${
-        sizes.width > 410 ? "h-[70px]" : "h-[45px]"
+        sizes.width > 800 ? "h-[70px]" : "h-[45px]"
       } bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600`}
     >
       <div className="grid h-full mx-auto grid-cols-5 font-medium">
         <div className="w-auto xl:w-[280px] flex justify-center items-center pl-2">
-          <div class="flex items-center">
+          <div class="cursor-pointer inline-flex flex-col items-center justify-center px-0 sm:px-2 hover:bg-gray-50 dark:hover:bg-gray-800 group">
             <a
               onClick={(e) => {
                 e.preventDefault();
@@ -39,11 +39,14 @@ export default function TabbarBottomChat({ active = "home" }) {
                 width={0}
                 height={0}
                 sizes="100vw"
-                src="/dumuc/logo.png"
-                class="w-full h-6 sm:h-10 mr-3 cursor-pointer"
+                src="/icons/bottom/Home.png"
+                class="w-full h-8 h-8 cursor-pointer"
                 alt="DuMuc"
               />
             </a>
+            <span class="text-base md:text-lg font-semibold text-gray-500 bottomTabBarHiden1">
+              Trang chủ
+            </span>
           </div>
         </div>
 
@@ -53,7 +56,7 @@ export default function TabbarBottomChat({ active = "home" }) {
             className="cursor-pointer inline-flex flex-col items-center justify-center px-0 sm:px-2 hover:bg-gray-50 dark:hover:bg-gray-800 group"
           >
             <MdPending size={32} color="#c80000" />
-            <span class="text-base md:text-lg font-semibold text-[#c80000] bottomTabBarHiden">
+            <span class="text-base md:text-lg font-semibold text-[#c80000] bottomTabBarHiden1">
               Chat
             </span>
           </Link>
@@ -63,7 +66,7 @@ export default function TabbarBottomChat({ active = "home" }) {
             className="cursor-pointer inline-flex flex-col items-center justify-center px-0 sm:px-2 hover:bg-gray-50 dark:hover:bg-gray-800 group"
           >
             <MdPending size={32} color="#9C9C9C" />
-            <span class="text-base md:text-lg font-semibold text-gray-500 bottomTabBarHiden">
+            <span class="text-base md:text-lg font-semibold text-gray-500 bottomTabBarHiden1">
               Chat
             </span>
           </Link>
@@ -74,7 +77,7 @@ export default function TabbarBottomChat({ active = "home" }) {
             className="cursor-pointer inline-flex flex-col items-center justify-center px-0 sm:px-2 hover:bg-gray-50 dark:hover:bg-gray-800 group"
           >
             <FaUserFriends size={32} color="#c80000" />
-            <span class="text-base md:text-lg font-semibold text-[#c80000] bottomTabBarHiden">
+            <span class="text-base md:text-lg font-semibold text-[#c80000] bottomTabBarHiden1">
               Bạn bè
             </span>
           </Link>
@@ -84,7 +87,7 @@ export default function TabbarBottomChat({ active = "home" }) {
             className="cursor-pointer inline-flex flex-col items-center justify-center px-0 sm:px-2 hover:bg-gray-50 dark:hover:bg-gray-800 group"
           >
             <FaUserFriends size={32} color="#9C9C9C" />
-            <span class="text-base md:text-lg font-semibold text-gray-500 bottomTabBarHiden">
+            <span class="text-base md:text-lg font-semibold text-gray-500 bottomTabBarHiden1">
               Bạn bè
             </span>
           </Link>
@@ -95,7 +98,7 @@ export default function TabbarBottomChat({ active = "home" }) {
             className="cursor-pointer inline-flex flex-col items-center justify-center px-0 sm:px-2 hover:bg-gray-50 dark:hover:bg-gray-800 group"
           >
             <MdOutlineSupervisedUserCircle size={32} color="#c80000" />
-            <span class="text-base md:text-lg font-semibold text-[#c80000] bottomTabBarHiden">
+            <span class="text-base md:text-lg font-semibold text-[#c80000] bottomTabBarHiden1">
               Nhóm cộng đồng
             </span>
           </Link>
@@ -105,7 +108,7 @@ export default function TabbarBottomChat({ active = "home" }) {
             className="cursor-pointer inline-flex flex-col items-center justify-center px-0 sm:px-2 hover:bg-gray-50 dark:hover:bg-gray-800 group"
           >
             <MdOutlineSupervisedUserCircle size={32} color="#9C9C9C" />
-            <span class="text-base md:text-lg font-semibold text-gray-500 bottomTabBarHiden">
+            <span class="text-base md:text-lg font-semibold text-gray-500 bottomTabBarHiden1">
               Nhóm cộng đồng
             </span>
           </Link>
@@ -116,7 +119,7 @@ export default function TabbarBottomChat({ active = "home" }) {
             className="cursor-pointer inline-flex flex-col items-center justify-center px-0 sm:px-2 hover:bg-gray-50 dark:hover:bg-gray-800 group"
           >
             <FaUsers size={32} color="#c80000" />
-            <span class="text-base md:text-lg font-semibold text-[#c80000] bottomTabBarHiden">
+            <span class="text-base md:text-lg font-semibold text-[#c80000] bottomTabBarHiden1">
               Nhóm riêng tư
             </span>
           </Link>
@@ -126,7 +129,7 @@ export default function TabbarBottomChat({ active = "home" }) {
             className="cursor-pointer inline-flex flex-col items-center justify-center px-0 sm:px-2 hover:bg-gray-50 dark:hover:bg-gray-800 group"
           >
             <FaUsers size={32} color="#9C9C9C" />
-            <span class="text-base md:text-lg font-semibold text-gray-500 bottomTabBarHiden">
+            <span class="text-base md:text-lg font-semibold text-gray-500 bottomTabBarHiden1">
               Nhóm riêng tư
             </span>
           </Link>
